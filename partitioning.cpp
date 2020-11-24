592d5328766f0fd2b862bb2b71c335938af3b75d
Capucine Leroux <capucine.leroux@polytechnique.edu>
2020-11-24 15:48:17 +0100

    Distortion error functions

--- partitioning.cpp
+++ partitioning.cpp
@@ -58,3 +58,64 @@ void fcolor(MatrixXd &Cf, MatrixXi Ad) {
   }
   
 }
+
+//******************Distortion error******************
+
+double triangle_area(Vector3d v1,Vector3d v2,Vector3d v3){
+
+  double l1 = (v2-v1).norm();
+  double l2 = (v3-v2).norm();
+  double l3 = (v1-v3).norm();
+  double p = (l1+l2+l3)/2.;
+  double S = pow(p*(p-l1)*(p-l2)*(p-l3),0.5);
+
+  return S;
+};
+
+double orthogonal_distance(pair<Vector3d , Vector3d> P, Vector3d M){
+
+  Vector3d X = P.first;
+  Vector3d N = P.second;
+
+  return fabs((N).dot(M-X))/N.norm();
+
+};
+
+Vector3d triangle_normal(Vector3d v1,Vector3d v2,Vector3d v3){
+
+  Vector3d N = (v3-v1).cross(v2-v3);
+  double n = N.norm();
+
+  return N/n;
+
+};
+
+double distance_L_2 (Vector3i T, pair<Vector3d , Vector3d> P, MatrixXd V){
+
+  Vector3d v1 = V.row(T(0));
+  Vector3d v2 = V.row(T(1));
+  Vector3d v3 = V.row(T(2));
+
+  double area = triangle_area(v1,v2,v3);
+  double d1 = orthogonal_distance(P,v1);
+  double d2 = orthogonal_distance(P,v2);
+  double d3 = orthogonal_distance(P,v3);
+
+  return (1/6.)*area*(d1*d1 + d2*d2 + d3*d3 + d1*d2 + d1*d3 + d2*d3);
+
+};
+
+double distance_L_2_1(Vector3i T, pair<Vector3d , Vector3d> P, MatrixXd V){
+
+  Vector3d v1 = V.row(T(0));
+  Vector3d v2 = V.row(T(1));
+  Vector3d v3 = V.row(T(2));
+
+  Vector3d N = P.second;
+
+  double area = triangle_area(v1,v2,v3);
+  Vector3d n = triangle_normal(v1,v2,v3);
+
+  return area*pow((n-N).norm(),2);
+
+};
--- partitioning.h
+++ partitioning.h
@@ -9,3 +9,12 @@ void tcolor(MatrixXi &Pf);
 void fcolor(MatrixXd &Cf, MatrixXi Ad);
 
 MatrixXi face_adjacency(MatrixXi F, int n);
+
+
+//******************Distortion error******************
+double triangle_area(Vector3d v1,Vector3d v2,Vector3d v3);
+double orthogonal_distance(pair<Vector3d , Vector3d> P, Vector3d M);
+Vector3d triangle_normal(Vector3d v1,Vector3d v2,Vector3d v3);
+
+double distance_L_2 (Vector3i T, pair<Vector3d , Vector3d> P, MatrixXd V);
+double distance_L_2_1(Vector3i T, pair<Vector3d , Vector3d> P, MatrixXd V);
