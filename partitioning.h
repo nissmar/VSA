0a575956603dc95a0237cce1f793e78c6b997014
Nissim <maruani.nissim@gmail.com>
2020-11-24 17:12:26 +0100

    color_proxies

--- partitioning.h
+++ partitioning.h
@@ -7,14 +7,17 @@ using namespace std;
 
 void tcolor(MatrixXi &Pf);
 void fcolor(MatrixXd &Cf, MatrixXi Ad);
+void distance_color(MatrixXd &Cf, MatrixXi F, MatrixXd V);
+void proxy_color(MatrixXi &Pf, MatrixXi Proxies, MatrixXd V, MatrixXi F, MatrixXi Ad);
 
+MatrixXi uniform_proxies(int k, int n);
 MatrixXi face_adjacency(MatrixXi F, int n);
 
 
 //******************Distortion error******************
 double triangle_area(Vector3d v1,Vector3d v2,Vector3d v3);
-double orthogonal_distance(pair<Vector3d , Vector3d> P, Vector3d M);
+double orthogonal_distance(Vector3d X, Vector3d N, Vector3d M);
 Vector3d triangle_normal(Vector3d v1,Vector3d v2,Vector3d v3);
 
-double distance_L_2 (Vector3i T, pair<Vector3d , Vector3d> P, MatrixXd V);
-double distance_L_2_1(Vector3i T, pair<Vector3d , Vector3d> P, MatrixXd V);
+double distance_L_2 (Vector3i T, Vector3d X, Vector3d N, MatrixXd V);
+double distance_L_2_1(Vector3i T, Vector3d N, MatrixXd V);
